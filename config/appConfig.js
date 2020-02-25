c4be32cb257b5000c5f17f9004cbd09a02e822fa
Vinit <vinitkumar7070@gmail.com>
2020-02-25 16:18:10 +0530

    updated

--- config/appConfig.js
+++ config/appConfig.js
@@ -4,7 +4,8 @@ appConfig.port = 3330;
 appConfig.allowedCorsOrigin = "*";
 appConfig.env = "env";
 appConfig.db = {
-    uri:"mongodb://127.0.0.1:27017/blogAppDB",
+    // uri:"mongodb://127.0.0.1:27017/blogAppDB",
+    uri: 'mongodb+srv://vinit:vinit2608@cluster0-sxxm9.mongodb.net/test?retryWrites=true&w=majority'
 }
 appConfig.apiVersion = "/api/v1";
 
